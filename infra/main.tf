c4d02de59190ea125917970087f44efb713fb0d2
Shen Wang <shengqiw97@gmail.com>
2024-10-08 20:03:02 -0500

    fix: thng

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,19 @@ provider "aws" {
     region = "us-east-1"
 }
 
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+    backend "s3" {
+        bucket = "clash-terraform-state"
+      
+    }
+}
+
 data "aws_iam_role" "lambda-role" {
     name = "lambdas"
 }
