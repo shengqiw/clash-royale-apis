8c09d92b47697c37f1b5926bea16670b07992711
Shen Wang <shengqiw97@gmail.com>
2024-10-09 19:33:17 -0500

    fix; typo

--- lambdas/get-user/index.mjs
+++ lambdas/get-user/index.mjs
@@ -1,8 +1,10 @@
 /* global fetch */
 export const getUser = async (event) => {
+    const unrefinedId = event.queryStringParameters?.id;
+
     console.log('hello from getUser', event.queryStringParameters);
 
-    if (!Object.keys(event.queryStringParameters).length) {
+    if (!unrefinedId) {
         return {
             statusCode: 400,
             headers: {
@@ -13,10 +15,9 @@ export const getUser = async (event) => {
     }
     
     const jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjMwZTg3ZmJjLTFjZTktNDAxZi05ZjgwLTAxMWE3MzFhNzcyNCIsImlhdCI6MTcyODUxNjgyMywic3ViIjoiZGV2ZWxvcGVyLzIxNjZhMjY0LTk2NjAtMTUxYi1iY2I0LTFiNTE1ODQzY2NiMiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIzNC4yMzAuMTYyLjE0NCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.L8iqiaQqKb5stziiXPZHppASSDX4vFgBse1x_IY7OJSBDgfjXamO3DnCmbAXB5c6w0MEvKBcDe-qGsZNXmXLJA";
-    
-    const playerId = event.queryStringParameters.id.trim().startsWith("#") ? 
-        event.id.trim() :
-        `#${event.id.trim()}`;
+    const playerId = unrefinedId.trim().startsWith("#") ? 
+        unrefinedId.trim() :
+        `#${unrefinedId.trim()}`;
     
     const res = await fetch(`https://api.clashroyale.com/v1/players/${playerId}`, {
         headers: {
