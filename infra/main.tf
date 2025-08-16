5e6d5f3b58f3db501d7b5b49e4fdad4b60eb65cd
Shen Wang <shengqiw97@gmail.com>
2025-08-16 15:03:28 -0500

    feat: updated tf state s3 name

--- infra/main.tf
+++ infra/main.tf
@@ -10,7 +10,7 @@ terraform {
         }
     }
     backend "s3" {
-        bucket = "clash-terraform-state"
+        bucket = "jeetio-clash-terraform-state"
         key = "lambdas/terraform.tfstate"
     }
 }
